c096ff075e0a0e2594b07694d2ee46e421e56f7c
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2019-03-04 15:01:45 +0100

    Make sure that the target of the test appears in the logs
    Use npm module "once" to log once

--- docker/acceptance/lib/urls.js
+++ docker/acceptance/lib/urls.js
@@ -1,5 +1,11 @@
+'use strict'
+
+const once = require('once'),
+      logOnce = once(console.log)
+
 module.exports = function urls () {
   let homesite = process.env.WP_ACCEPTANCE_TARGET || 'https://wp-httpd/'
+  logOnce('Testing at ' + homesite)
 
   return {
     home: homesite,
