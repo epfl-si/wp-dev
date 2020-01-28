9f5802ca018b18d5e31f9c3e03e64c4a0a711f7e
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2020-01-28 15:31:41 +0100

    [feature] Script to copy a hierarchy of WordPress sites from production
    - Uses `wp db dump` / `wp db restore` + `wp search-replace`, as per
      https://confluence.epfl.ch:8443/pages/viewpage.action?pageId=75662209
    - Symlinks sites in a crude fashion (enough for EPFL plug-ins and
      themes to load)

---
+++ devscripts/copy-enac-from-prod.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+set -e -x
+scriptdir="$(dirname "$0")"
+
+wp_export_prod() {
+    ssh -X www-data@ssh-wwp.epfl.ch -p 32222 "cd /srv/www/www.epfl.ch/htdocs/$1; wp db export -"
+}
+
+docker_mgmt_cmd () {
+    if [ -z "$_mgmt_container" ]; then
+        _mgmt_container="$(. "$scriptdir"/../.env; docker ps -q --filter "label=ch.epfl.wordpress.mgmt.env=${WP_ENV}")"
+    fi
+    docker exec -i --user www-data "$_mgmt_container" bash -c "$*"
+}
+
+wp_import_docker() {
+    local site="$1"
+    docker_mgmt_cmd "set -e -x; cd /srv/test/wp-httpd/htdocs/$site; pwd; wp db import -"
+}
+
+wp_prepare () {
+    local site="$1"
+    docker_mgmt_cmd "$(cat <<CREATE_SITE_SCRIPT
+set -e -x
+mkdir -p /srv/test/wp-httpd/htdocs/$site || true
+cd /srv/test/wp-httpd/htdocs/$site
+/usr/local/bin/new-wp-site
+CREATE_SITE_SCRIPT
+
+cat <<'POPULATE_SITE_SCRIPT'
+rm wp
+ln -s /wp/5.2 wp
+for symlinkdir in wp-content/plugins wp-content/mu-plugins wp-content/themes; do
+    mkdir -p "$symlinkdir" || true
+    rm -f "$symlinkdir"/*
+    (cd "$symlinkdir"; pwd; ln -s ../../wp/"$symlinkdir"/* .)
+done
+
+POPULATE_SITE_SCRIPT
+)"
+}
+
+wp_search_replace () {
+    local site="$1"
+    docker_mgmt_cmd "set -e -x; cd /srv/test/wp-httpd/htdocs/$site; wp search-replace https://www.epfl.ch http://wp-httpd"
+}
+
+# TODO: make this flexible (split $1, reverse)
+for site in "" schools schools/enac schools/enac/education3 ; do
+    wp_prepare "$site"
+    wp_export_prod "$site" | wp_import_docker "$site"
+    wp_search_replace "$site"
+done
