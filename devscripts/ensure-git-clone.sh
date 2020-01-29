3f5dfc95e6ba2c1cfd6abb97610d451497f56fbb
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2020-01-29 11:02:46 +0100

    [feature] Implement cases where TARGET_DIR is a symlink or an old checkout
    This is required for future Git repository re-hosting operations, such
    as this one, to occur seamlessly. People only have to
    
    ```
    touch volumes/wp/5/wp-content
    ```

--- devscripts/ensure-git-clone.sh
+++ devscripts/ensure-git-clone.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
 ## Usage : 
 ## ensure-git-clone.sh <gitURL> <directory>
-test -d "$2" || git clone "$1" "$2"
+
+set -e -x
+
+GIT_URL="$1"
+TARGET_DIR="$2"
+
+do_git_clone() {
+    git clone "$GIT_URL" "$TARGET_DIR"
+}
+
+if test -L "$TARGET_DIR"; then
+    # Was probably a link to a module under jahia2wp, which got re-hosted.
+    rm "$TARGET_DIR"
+fi
+
+if ! test -d "$TARGET_DIR"; then
+    do_git_clone
+elif [ "$(cd "$TARGET_DIR" && git remote get-url origin)" != "$GIT_URL" ] ; then
+    mv "$TARGET" "$TARGET"-orig
+    do_git_clone
+fi
