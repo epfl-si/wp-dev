e8d3729dc71c87563adca6c15b219df4e6e210be
Julien Delasoie <julien.delasoie@epfl.ch>
2020-12-16 15:14:23 +0100

    Set a better way to login

--- acceptance/features/steps_definition/authentication_steps.js
+++ acceptance/features/steps_definition/authentication_steps.js
@@ -1,20 +1,13 @@
 const { Given, When, Then } = require('cucumber'),
-  assert = require('assert')
+    assert = require('assert')
 
 async function admin_login(context) {
     await context.page.goto(context.urls.login)
 
-    //FIXME: which one of credentials is used depends on url, maybe
-    await context.page.focus('#user_login')
-    await context.page.keyboard.down('Control')
-    await context.page.keyboard.press('a')
-    await context.page.keyboard.up('Control')
-    await context.page.keyboard.press('Delete')
+    //TODO: get the right credentials, depending of where it is launched
+    await context.page.$eval('input[name=log]', (el, value) => el.value = value, context.credentials.wordpress_admin_local.user);
+    await context.page.$eval('input[name=pwd]', (el, value) => el.value = value, context.credentials.wordpress_admin_local.password);
 
-    await context.page.keyboard.type(context.credentials.wordpress_admin_local.user)
-
-    await context.page.focus('#user_pass')
-    await context.page.keyboard.type(context.credentials.wordpress_admin_local.password)
     await context.page.click("#wp-submit")
 }
 
