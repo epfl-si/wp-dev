0c6c4855808c4c5e333a88248185266da00576e1
Nicolas Borboën <ponsfrilus@gmail.com>
2023-06-26 13:10:57 +0200

    [refactor] local-restore-from-restic + named container
    Is now more generic: it can be run from anywhere and use the `WP_ENV` 
    variables.
    Restic secrets are exported otherwise the restic command is not running 
    unless you already have them in cache (`~/.cache/restic`).

--- devscripts/local-restore-from-restic.sh
+++ devscripts/local-restore-from-restic.sh
@@ -7,50 +7,51 @@
 # SITE_ORIGINAL_URL=https://www.epfl.ch/campus/services/website/canari/ SITE_ANSIBLE_IDENTIFIER=www__campus__services__website__canari RESTORED_SITE_DIR_NAME=canari ./local-restore-from-restic.sh
 #
 
-# TODO
-#  - manage secret in a better way (shell script?)
+# TODO:
 #  - ensure that everythings is here (keybase, restic, etc.)
 #  - improve wp-veritas to be able to fetch `ansibleHost` with a query
 #  - not chmod 777
-#  - set the container name and/or query it (`wp-local-mgmt-1`)
-#  - improve script args
 #
 
 set -e -x
 
+scriptdir="$(dirname "$0")"
+export $(cat ${scriptdir}/../.env | grep WP_ENV=)
+_mgmt_container="$(docker ps -q --filter "label=ch.epfl.wordpress.mgmt.env=${WP_ENV}")"
+
 SITE_ORIGINAL_URL="${SITE_ORIGINAL_URL:-https://www.epfl.ch/campus/services/website/canari/}"
 SITE_ANSIBLE_IDENTIFIER="${SITE_ANSIBLE_IDENTIFIER:-www__campus__services__website__canari}"
 RESTORED_SITE_DIR_NAME="${RESTORED_SITE_DIR_NAME:-canari}"
+S3_BUCKET_NAME="${S3_BUCKET_NAME:-svc0041-df3298778888f91b2b62cf913f4c8c74}"
 
-AWS_DEFAULT_REGION=us-east-1 # This is the default
+export AWS_DEFAULT_REGION=us-east-1 # This is the default
 # Retrieve AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID and RESTIC_PASSWORD from keybase
-AWS_SECRET_ACCESS_KEY=$(cat /keybase/team/epfl_wp_prod/aws-cli-credentials | grep -A2 '\[backup-wwp\]' | grep aws_secret_access_key | sed 's/aws_secret_access_key = //')
-AWS_ACCESS_KEY_ID=$(cat /keybase/team/epfl_wp_prod/aws-cli-credentials | grep -A2 '\[backup-wwp\]' | grep aws_access_key_id | sed 's/aws_access_key_id = //')
-RESTIC_PASSWORD=$(cat /keybase/team/epfl_wp_prod/aws-cli-credentials | grep -A3 '\[backup-wwp\]' | grep restic_password | sed 's/restic_password = //')
-
+export AWS_SECRET_ACCESS_KEY=$(cat /keybase/team/epfl_wp_prod/aws-cli-credentials | grep -A2 '\[backup-wwp\]' | grep aws_secret_access_key | sed 's/aws_secret_access_key = //')
+export AWS_ACCESS_KEY_ID=$(cat /keybase/team/epfl_wp_prod/aws-cli-credentials | grep -A2 '\[backup-wwp\]' | grep aws_access_key_id | sed 's/aws_access_key_id = //')
+export RESTIC_PASSWORD=$(cat /keybase/team/epfl_wp_prod/aws-cli-credentials | grep -A3 '\[backup-wwp\]' | grep restic_password | sed 's/restic_password = //')
 
 # Get the latest DB backup from S3
-restic -r s3:https://s3.epfl.ch/svc0041-df3298778888f91b2b62cf913f4c8c74/backup/wordpresses/${SITE_ANSIBLE_IDENTIFIER}/sql dump latest db-backup.sql > ../volumes/srv/test/${SITE_ANSIBLE_IDENTIFIER}-db-backup.sql
+restic -r s3:https://s3.epfl.ch/${S3_BUCKET_NAME}/backup/wordpresses/${SITE_ANSIBLE_IDENTIFIER}/sql dump latest db-backup.sql > ${scriptdir}/../volumes/srv/${WP_ENV}/${SITE_ANSIBLE_IDENTIFIER}-db-backup.sql
 
 # Create the empty dir for the new site
-docker exec --user www-data -i wp-local-mgmt-1 bash -c "mkdir -p /srv/test/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME}"
+docker exec --user www-data -i ${_mgmt_container} bash -c "mkdir -p /srv/${WP_ENV}/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME}"
 
 # Initilialize the new site
-docker exec --user www-data -i wp-local-mgmt-1 bash -c "cd /srv/test/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME}; new-wp-site --debug"
+docker exec --user www-data -i ${_mgmt_container} bash -c "cd /srv/${WP_ENV}/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME}; new-wp-site --debug"
 
 # How to deal with local user right knowing that it's www-data in the container, this is very dirty
-sudo chmod 777 -R ../volumes/srv/test/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME}/wp-content
+sudo chmod 777 -R ${scriptdir}/../volumes/srv/${WP_ENV}/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME}/wp-content
 
 # Restore the backup directly in the new site's folder
-restic -r s3:https://s3.epfl.ch/svc0041-df3298778888f91b2b62cf913f4c8c74/backup/wordpresses/${SITE_ANSIBLE_IDENTIFIER}/files restore latest \
+restic -r s3:https://s3.epfl.ch/${S3_BUCKET_NAME}/backup/wordpresses/${SITE_ANSIBLE_IDENTIFIER}/files restore latest \
             --include="/wp-content" \
-            --target ../volumes/srv/test/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME}/
+            --target ${scriptdir}/../volumes/srv/${WP_ENV}/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME}/
 
 # Import the DB
-docker exec --user www-data -i wp-local-mgmt-1 bash -c "wp --path=/srv/test/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME} db import /srv/test/${SITE_ANSIBLE_IDENTIFIER}-db-backup.sql"
+docker exec --user www-data -i ${_mgmt_container} bash -c "wp --path=/srv/${WP_ENV}/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME} db import /srv/${WP_ENV}/${SITE_ANSIBLE_IDENTIFIER}-db-backup.sql"
 
 # Ensure that URLs are correct with search-replace
-docker exec --user www-data -i wp-local-mgmt-1 bash -c "wp --path=/srv/test/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME} search-replace ${SITE_ORIGINAL_URL} https://wp-httpd/${RESTORED_SITE_DIR_NAME}"
+docker exec --user www-data -i ${_mgmt_container} bash -c "wp --path=/srv/${WP_ENV}/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME} search-replace ${SITE_ORIGINAL_URL} https://wp-httpd/${RESTORED_SITE_DIR_NAME}"
 
 # Set the admin password to "secret"
-docker exec --user www-data -i wp-local-mgmt-1 bash -c "wp --path=/srv/test/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME} user update admin --user_pass=secret"
+docker exec --user www-data -i ${_mgmt_container} bash -c "wp --path=/srv/${WP_ENV}/wp-httpd/htdocs/${RESTORED_SITE_DIR_NAME} user update admin --user_pass=secret"
