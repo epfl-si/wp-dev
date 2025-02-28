f34a1dba0fee338de1af19c9c4ccdba38ca487df
Nicolas Borboën <ponsfrilus@gmail.com>
2025-02-28 15:08:53 +0100

    [feature](WIP) Local dev
    This an intermediate commit in middle of everything

--- devscripts/ensure-git-clone.sh
+++ devscripts/ensure-git-clone.sh
@@ -1,24 +1,26 @@
-#!/bin/sh
-## Usage : 
-## ensure-git-clone.sh <gitURL> <directory>
+#!/usr/bin/env bash
+## Usage:
+##   ensure-git-clone.sh <repository> <destination> <branch>
 
-set -e -x
+#set -e -x
 
-GIT_URL="$1"
+REPOSITORY_URL="$1"
 TARGET_DIR="$2"
+TARGET_BRANCH="${3:-main}"
 
 do_git_clone() {
-    git clone "$GIT_URL" "$TARGET_DIR"
+    echo "$REPOSITORY_URL → $TARGET_DIR | $TARGET_BRANCH"
+    git clone -b "$TARGET_BRANCH" "$REPOSITORY_URL" "$TARGET_DIR"
 }
 
 if test -L "$TARGET_DIR"; then
-    # Was probably a link to a module under jahia2wp, which got re-hosted.
+    # Clean up symlink if exists
     rm "$TARGET_DIR"
 fi
 
 if ! test -d "$TARGET_DIR"; then
     do_git_clone
-elif [ "$(cd "$TARGET_DIR" && git remote get-url origin)" != "$GIT_URL" ] ; then
+elif [ "$(cd "$TARGET_DIR" && git remote get-url origin)" != "$REPOSITORY_URL" ] ; then
     mv "$TARGET_DIR" "$TARGET_DIR"-orig
     do_git_clone
 fi
