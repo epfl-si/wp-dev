5f619cbf540e6423367fced80f553158873888cf
Dominique Quatravaux <dominique@quatravaux.org>
2025-07-07 12:41:40 +0200

    [deadcode] Automated UI tests

--- acceptance/features/page-accueil.feature
+++
@@ -1,6 +0,0 @@
-# language: fr
-Fonctionnalité: Page d'accueil
-
-  Scénario: Page d'accueil
-    Lorsque je navigue vers la page d'accueil
-    Alors je n'ai pas d'erreurs
--- acceptance/lib/credentials.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function get(world) {
-  return {
-    wordpress_admin: {
-      user: 'kermit',
-      password: process.env.KERMIT_PASSWORD
-    }
-  }
-}
